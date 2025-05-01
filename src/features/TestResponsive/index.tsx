f94ab4927f371bd7a015fe670641578a16e617c1
solssak <gswoong1012@gmail.com>
2025-05-02 01:14:24 +0900

    feat: TestResponsive 컴포넌트 추가
    - padding mixin 동작 확인용 TestResponsive 컴포넌트 추가

---
+++ src/features/TestResponsive/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+export default function TestResponsive() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Daggle</h1>
+      </div>
+    </div>
+  );
+}
