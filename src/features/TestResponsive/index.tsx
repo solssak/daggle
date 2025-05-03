916ffd143ebb657f43e09b02cc276386145ddb7a
solssak <gswoong1012@gmail.com>
2025-05-03 22:29:01 +0900

    feat: 버튼 공용 컴포넌트 구현

--- src/features/TestResponsive/index.tsx
+++ src/features/TestResponsive/index.tsx
@@ -1,10 +1,23 @@
+import Button from '@/ui/Button/Button';
 import styles from './index.module.scss';
 
 export default function TestResponsive() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h1>Daggle</h1>
+        <h1>Button example</h1>
+        <Button variant="grayscale1" size="lg">
+          버튼
+        </Button>
+        <Button variant="purple" size="sm">
+          버튼
+        </Button>
+        <Button variant="grayscale1" size="sm">
+          버튼
+        </Button>
+        <Button variant="grayscale1" size="md" disabled>
+          버튼
+        </Button>
       </div>
     </div>
   );
