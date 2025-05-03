d641da09d03c76b166661eef3a0c8485bcbbfab2
solssak <gswoong1012@gmail.com>
2025-05-04 07:40:10 +0900

    feat: 헤더 컴포넌트 로그인 여부에 따른 UI 변경
    - 로그인 시 프로필 아이콘 표시, 비로그인 시 로그인 버튼 표시

--- src/features/ui/Header/Header.tsx
+++ src/features/ui/Header/Header.tsx
@@ -1,11 +1,22 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './Header.module.scss';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+    setIsLogin(!!accessToken && !!refreshToken);
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <div className={styles.header__logo}>
+        <div className={styles.header__container__logo}>
           <Image
             src="/images/logo/logo@3x.png"
             alt="logo"
@@ -15,7 +26,18 @@ export default function Header() {
         </div>
         <div className={styles.header__isLogin}>
           <div className={styles.header__isLogin__button}>
-            <button>로그인</button>
+            {isLogin ? (
+              <Image
+                src="/images/auth/profile.svg"
+                alt="profile"
+                width={31.61}
+                height={31.61}
+              />
+            ) : (
+              <button className={styles.header__isLogin__button__login}>
+                로그인
+              </button>
+            )}
           </div>
         </div>
       </div>
