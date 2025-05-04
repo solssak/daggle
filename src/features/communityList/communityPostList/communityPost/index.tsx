538daec7db9582f4ba10d43b042962d8c98b0841
solssak <gswoong1012@gmail.com>
2025-05-04 22:34:52 +0900

    feat: 커뮤니티 상세 링크 추가

--- src/features/communityList/communityPostList/communityPost/index.tsx
+++ src/features/communityList/communityPostList/communityPost/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './index.module.scss';
 
 export interface CommunityPostProps {
@@ -18,37 +19,41 @@ export default function CommunityPost({
 }: CommunityPostProps) {
   return (
     <li className={styles.container} key={id}>
-      <article className={styles.container__content}>
-        <h3 className={styles.container__content__title}>{title}</h3>
-        <footer className={styles.container__content__meta}>
-          <time
-            className={styles.container__content__meta__date}
-            dateTime={createdAt}
-          >
-            {createdAt?.slice(2, 10).replace(/-/g, '.')}
-          </time>
-          <div className={styles.container__content__meta__item__comment}>
-            <Image
-              src={'/images/community-list/comment.svg'}
-              alt="comment"
-              width={19}
-              height={18}
-            />
-            <span
-              className={styles.container__content__meta__item__comment__count}
+      <Link href={`/post/${id}`} className={styles.container__link}>
+        <article className={styles.container__content}>
+          <h3 className={styles.container__content__title}>{title}</h3>
+          <footer className={styles.container__content__meta}>
+            <time
+              className={styles.container__content__meta__date}
+              dateTime={createdAt}
             >
-              {commentCount}
-            </span>
-          </div>
-          <Image
-            className={styles.container__content__meta__item__profile}
-            src={'/images/community-list/profile.svg'}
-            alt="profile"
-            width={24}
-            height={24}
-          />
-        </footer>
-      </article>
+              {createdAt?.slice(2, 10).replace(/-/g, '.')}
+            </time>
+            <div className={styles.container__content__meta__item__comment}>
+              <Image
+                src={'/images/community-list/comment.svg'}
+                alt="comment"
+                width={19}
+                height={18}
+              />
+              <span
+                className={
+                  styles.container__content__meta__item__comment__count
+                }
+              >
+                {commentCount}
+              </span>
+            </div>
+            <Image
+              className={styles.container__content__meta__item__profile}
+              src={'/images/community-list/profile.svg'}
+              alt="profile"
+              width={24}
+              height={24}
+            />
+          </footer>
+        </article>
+      </Link>
     </li>
   );
 }
