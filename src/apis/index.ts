fe45fb8f1306d12953286e521e67d5c2e3aae6ea
solssak <gswoong1012@gmail.com>
2025-05-05 22:37:22 +0900

    feat: axios interceptor 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,3 +4,11 @@ import { API_URL } from '@/constants/env';
 export const apiCaller: AxiosInstance = axios.create({
   baseURL: API_URL,
 });
+
+apiCaller.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
