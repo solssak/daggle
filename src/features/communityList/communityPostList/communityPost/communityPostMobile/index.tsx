508c00c1748e942d07a40909d54030ae73f2dc04
solssak <gswoong1012@gmail.com>
2025-05-09 17:30:13 +0900

    style: 커뮤니티 리스트 반응형 작업

---
+++ src/features/communityList/communityPostList/communityPost/communityPostMobile/index.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './index.module.scss';
+
+export interface CommunityPostMobileProps {
+  id: string;
+  title: string;
+  createdAt: string;
+  commentCount: number;
+  username: string;
+}
+
+export default function CommunityPostMobile({
+  id,
+  title,
+  createdAt,
+  commentCount,
+  username,
+}: CommunityPostProps) {
+  return (
+    <li className={styles.container} key={id}>
+      <Link href={`/post/${id}`} className={styles.container__link}>
+        <article className={styles.container__content}>
+          <h3 className={styles.container__content__title}>{title}</h3>
+          <div className={styles.container__content__bottom}>
+            <div className={styles.container__content__bottom__meta}>
+              <time
+                className={styles.container__content__bottom__meta__date}
+                dateTime={createdAt}
+              >
+                {createdAt?.slice(2, 10).replace(/-/g, '.')}
+              </time>
+              <div className={styles.container__content__bottom__meta__comment}>
+                <Image
+                  src={'/images/community-list/comment.svg'}
+                  alt="comment"
+                  width={16}
+                  height={16}
+                />
+                <span
+                  className={
+                    styles.container__content__bottom__meta__comment__count
+                  }
+                >
+                  {commentCount}
+                </span>
+              </div>
+            </div>
+            <div className={styles.container__content__bottom__profile}>
+              <Image
+                src={'/images/community-list/profile.svg'}
+                alt="profile"
+                width={24}
+                height={24}
+              />
+              <span className={styles.container__content__bottom__username}>
+                {username || '익명유저'}
+              </span>
+            </div>
+          </div>
+        </article>
+      </Link>
+    </li>
+  );
+}
--- src/features/communityList/communityPostList/index.tsx
+++ src/features/communityList/communityPostList/index.tsx
@@ -8,6 +8,8 @@ import styles from './index.module.scss';
 import PageSelector from './pageSelector';
 import { useRouter } from 'next/navigation';
 import { useMyInfoStore } from '@/globalState/zusatnd/useMyInfoStore';
+import useMedia from '@/hooks/useMedia';
+import CommunityPostMobile from './communityPost/communityPostMobile';
 
 export default function CommunityPostList() {
   const [page, setPage] = useState<number>(1);
@@ -15,6 +17,7 @@ export default function CommunityPostList() {
   const { data } = useGetCommunityList(page, limit);
   const router = useRouter();
   const { accessToken } = useMyInfoStore();
+  const { isMobile } = useMedia();
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
@@ -33,29 +36,44 @@ export default function CommunityPostList() {
       {/* 게시판 헤더 */}
       <div className={styles.container__header}>
         <span className={styles.container__header__title}>게시판</span>
-        <Button variant="purple" onClick={handleWriteClick}>
-          글쓰기
-        </Button>
+        {!isMobile && (
+          <Button variant="purple" onClick={handleWriteClick}>
+            글쓰기
+          </Button>
+        )}
       </div>
       {/* 게시판 목록 */}
       <ul className={styles.container__list}>
-        {data?.items.map((item) => (
-          <CommunityPost
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            createdAt={item.createdAt}
-            commentCount={item.commentCount}
-          />
-        ))}
+        {data?.items.map((item) =>
+          !isMobile ? (
+            <CommunityPost
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              createdAt={item.createdAt}
+              commentCount={item.commentCount}
+            />
+          ) : (
+            <CommunityPostMobile
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              createdAt={item.createdAt}
+              commentCount={item.commentCount}
+              username={item.username}
+            />
+          ),
+        )}
       </ul>
 
       {/* 페이지 선택 */}
-      <PageSelector
-        currentPage={page}
-        totalPages={data?.meta.totalPages || 1}
-        onPageChange={handlePageChange}
-      />
+      {!isMobile && (
+        <PageSelector
+          currentPage={page}
+          totalPages={data?.meta.totalPages || 1}
+          onPageChange={handlePageChange}
+        />
+      )}
     </div>
   );
 }
